e552b398c3f48bce3eb8adc26a12d15e53415cc7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-23 00:23:44 +0600

    update app config

--- application/config/app_config.py
+++ application/config/app_config.py
@@ -2,11 +2,11 @@ from pweb import PWebAppConfig
 
 
 class Config(PWebAppConfig):
-    APP_NAME = "pweb-crud-example"
+    APP_NAME = "Hello App"
     PORT: int = 1212
     
-    CONNECT_MESSAGE: str = "PWeb is an Open Source Python based Web Framework for Rapid Development."
-    DEVELOPED_BY: str = "Bangla Fighter Engineering"
-    DEVELOPED_BY_LINK: str = "https://banglafighter.com"
-    APP_VERSION: str = "v1.0.0"
+    CONNECT_MESSAGE: str = "Hello App is an Open Source Python based Web Framework for Rapid Development."
+    DEVELOPED_BY: str = "Najatul islam engineer"
+    DEVELOPED_BY_LINK: str = "https://najatul-islam.vercel.app"
+    APP_VERSION: str = "v2.0.0"
     ENABLE_REGISTRATION: bool = True
