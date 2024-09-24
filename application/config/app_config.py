fabb3d3f6dc261e89aeaf04c1a77b48cb393bc90
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-24 22:16:41 +0600

    change apps name

--- application/config/app_config.py
+++ application/config/app_config.py
@@ -2,11 +2,11 @@ from pweb import PWebAppConfig
 
 
 class Config(PWebAppConfig):
-    APP_NAME = "Hello App"
+    APP_NAME = "Hello App with student list"
     PORT: int = 1212
     
     CONNECT_MESSAGE: str = "Hello App is an Open Source Python based Web Framework for Rapid Development."
-    DEVELOPED_BY: str = "Najatul islam engineer"
+    DEVELOPED_BY: str = "Najatul islam"
     DEVELOPED_BY_LINK: str = "https://najatul-islam.vercel.app"
     APP_VERSION: str = "v2.0.0"
     ENABLE_REGISTRATION: bool = True
